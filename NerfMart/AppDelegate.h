f2e784c0ce2e7b291959d59be15de6f4c39facff
= <=>
2018-09-06 16:01:36 -0700

    Initial Commit

---
+++ NerfMart/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NerfMart
+//
+//  Created by Dale on 4/3/17.
+//  Copyright © 2017 Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Library/Screen Record/CaptureRecord.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
+
